4d11f5ba559203d9df9636cb9fd9219c05d11115
Mars182838 <wj182838@126.com>
2013-04-11 17:41:44 +0800

    Initial Commit

---
+++ CarmerSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CarmerSample
+//
+//  Created by Mars on 13-4-11.
+//  Copyright (c) 2013年 Mars. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
